60bd6872ff9f51c7b9f2cfcec4428d830fbef6a7
albertescanes <albert.escanesespinosa@gmail.com>
2025-10-15 00:09:22 +0200

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -xeuo pipefail
+
+dnf config-manager setopt fedora-cisco-openh264.enabled=1
+
+dnf -y install \
+    https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
+    https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm \
+    fastfetch \
+    steam-devices \
+    ffmpegthumbnailer
+
+dnf -y swap \
+    ffmpeg-free ffmpeg --allowerasing
+
+dnf -y remove \
+    firefox-langpacks \
+    firefox \
+    gnome-shell-extension-{apps-menu,launch-new-instance,places-menu,window-list,background-logo}
+
+dnf -y clean all
